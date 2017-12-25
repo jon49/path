9668b2e77fd39c67de9a77fd0e3bffa7eeef82b3
Jon Nyman <jon.nyman@offerpad.com>
2017-12-25 01:39:06 -0700

    Added soundcloud

--- fsx/yt.fsx
+++ fsx/yt.fsx
@@ -11,12 +11,14 @@ open Shell.Shell
 let dir = __SOURCE_DIRECTORY__
 let userDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
 let media = Path.Combine(userDir, "Downloads", "media")
+let keep = Path.Combine(media, "keep")
 
 let ``youtube-dl`` = Path.Combine(dir, "../youtube-dl", "youtube-dl.exe")
 
 type YoutubeChoice =
     | LQ_MP3
     | HQ_MP3
+    | HQ_MP3_SoundCloud
     | HQ_Facebook
 
 let youtube choice links =
@@ -25,13 +27,15 @@ let youtube choice links =
     match choice with
     | LQ_MP3 -> audio 9 links
     | HQ_MP3 -> audio 1 links
+    | HQ_MP3_SoundCloud -> sprintf """ -f http_mp3_128_url -x --audio-format mp3 --audio-quality 1 %s %s""" title links
     | HQ_Facebook -> sprintf """ -f dash_sd_src_no_ratelimit %s %s""" title links
 
 let choiceEnumerated =
     Map(
         [| ("1", LQ_MP3)
            ("2", HQ_MP3)
-           ("3", HQ_Facebook) |] )
+           ("3", HQ_MP3_SoundCloud)
+           ("4", HQ_Facebook) |] )
 
 let printChoices =
     choiceEnumerated
@@ -40,9 +44,13 @@ let printChoices =
 let getChoice (num : string) =
     match choiceEnumerated.TryFind (num.Trim()) with
     | Some choice -> choice
-    | _ -> failwith "Can only choose 1, 2, 3, or 4."
+    | _ -> failwith (sprintf "Can only choose %s." (choiceEnumerated |> Map.toArray |> Array.map (fun (x, _) -> x) |> String.concat ", "))
 
 do
+
+    // if directory doesn't exit create it.
+    IO.Directory.CreateDirectory(keep) |> ignore
+
     printfn "
 Choose file type:
 %s"  printChoices
@@ -51,7 +59,7 @@ Choose file type:
     let links = Console.ReadLine () |> fun x -> x.Trim()
     Shell.execute
         { (Shell.create ``youtube-dl``) with
-            WorkingDirectory = Some media
+            WorkingDirectory = Some (if choice = LQ_MP3 then media else keep)
             Arguments = (youtube choice links)
             UseShellExecute = true
             WaitForExit = true }
