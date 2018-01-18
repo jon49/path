d4c696c64ed795d6fc5cf04c859c025c56005394
Jon Nyman <jon.nyman@offerpad.com>
2018-01-18 07:23:32 -0700

    cd to normal work location. doesn't work right now.

--- work.bat
+++ work.bat
@@ -1,1 +1,17 @@
-fsi "%~dp0fsx\work.fsx" %*
+set args=%*
+fsi "%~dp0fsx\work.fsx" %args%
+
+rem https://stackoverflow.com/a/7006016/632495
+if not x%args:me (
+cd C:\r\Platform
+) else (
+cd C:\s
+)
+
+rem https://stackoverflow.com/a/14275800/632495
+rem @echo off
+rem set /p option=Enter (y) if you would like to change directory:
+rem if %option%==y (
+rem cd C:\r\Platform
+rem )
+
